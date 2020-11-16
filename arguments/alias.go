8a045ab07841c754145832eaccdcf94d07590b51
Jon Inge Moe <jim@sol.no>
2020-11-16 17:09:36 +0100

    Validating first and last char in <path> arg

--- arguments/alias.go
+++ arguments/alias.go
@@ -11,10 +11,10 @@ const ValidAliasChars = "a-z-"
 
 func (a *Alias) Validate() error {
 	regStr := fmt.Sprintf(`^[%s]+$`, ValidAliasChars)
-	if regexp.MustCompile(regStr).MatchString(a.String()) {
-		return nil
+	if !regexp.MustCompile(regStr).MatchString(a.String()) {
+		return fmt.Errorf("illegal character in <aliases> (%s)", ValidAliasChars)
 	}
-	return fmt.Errorf("illegal character in <aliases> (%s)", ValidAliasChars)
+	return nil
 }
 
 func (a *Alias) String() string {
--- arguments/path.go
+++ arguments/path.go
@@ -1,6 +1,7 @@
 package arguments
 
 import (
+	"errors"
 	"fmt"
 	"regexp"
 )
@@ -11,12 +12,26 @@ const ValidPathChars = "a-zA-Z0-9-_/."
 
 func (p *Path) Validate() error {
 	regStr := fmt.Sprintf(`^[%s]+$`, ValidPathChars)
-	if regexp.MustCompile(regStr).MatchString(p.String()) {
-		return nil
+	if !regexp.MustCompile(regStr).MatchString(p.String()) {
+		return fmt.Errorf("illegal character in <path> (%s)", ValidPathChars)
 	}
-	return fmt.Errorf("illegal character in <path> (%s)", ValidPathChars)
+
+	if p.subStr(0, 1) != "/" {
+		return errors.New("first char in <path> must be '/'")
+	}
+
+	length := len(p.String())
+	if p.subStr(length-1, length) == "/" {
+		return errors.New("the <path> should not end with '/'")
+	}
+
+	return nil
 }
 
 func (p *Path) String() string {
 	return string(*p)
+}
+
+func (p *Path) subStr(first, last int) string {
+	return string([]rune(p.String())[first:last])
 }
