fda4cc2459da516a04405c2f12510364ac0a3fc5
Jon Inge Moe <jim@sol.no>
2020-11-18 21:49:01 +0100

    ...and removed the old conf

--- config/cli.go
+++ config/cli.go
@@ -3,15 +3,11 @@ package config
 type CliConfig struct {
 	CliName string
 	Version string
-	// SourceHome string
-	// BinHome    string
 }
 
 func getCliConfig() *CliConfig {
 	return &CliConfig{
 		CliName: "editorAndChangeDir",
 		Version: "v1.0.0",
-		// SourceHome: "/home/jim/code/privat/editor-and-change-dir/",
-		// BinHome:    "/home/jim/bin/",
 	}
 }
