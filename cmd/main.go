05676a0ad5294f744c29380488700a29d3a3219d
Jon Inge Moe <jim@sol.no>
2020-12-21 21:52:42 +0100

    Allow 'file not found' error from yaml

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 
@@ -11,9 +13,20 @@ import (
 
 func main() {
 	cfg, err := config.Load()
-	if err != nil {
+	if err != nil && isFatalError(err) {
 		log.Fatal(fmt.Errorf("config: %w", err))
 	}
 
 	commands.Execute(cfg)
+}
+
+func isFatalError(err error) bool {
+	var e *os.PathError
+	if errors.As(err, &e) {
+		if os.IsNotExist(e.Unwrap()) {
+			return false
+		}
+	}
+
+	return true
 }
--- config/config.go
+++ config/config.go
@@ -19,22 +19,24 @@ type Config struct {
 	*YamlConfig
 }
 
-func Load() (*Config, error) {
-	userConfigDir, err := os.UserConfigDir()
-	if err != nil {
-		return &Config{}, fmt.Errorf("could not get os user-specific config directory. Message: %w", err)
-	}
-
-	cfg := &Config{}
+func Load() (cfg *Config, err error) {
+	cfg = &Config{}
 	cfg.CliName = "repocli"
 	cfg.Version = "v1.0.0"
+
+	userConfigDir, err := os.UserConfigDir()
+	if err != nil {
+		return cfg, fmt.Errorf("could not get os user-specific config directory. Message: %w", err)
+	}
+
 	cfg.Yaml.Filename = "config.yml"
 	cfg.Yaml.Path = fmt.Sprintf("%s/%s", userConfigDir, cfg.CliName)
 	cfg.Yaml.PathAndFilename = fmt.Sprintf("%s/%s", cfg.Yaml.Path, cfg.Yaml.Filename)
 
 	yaml, err := loadYaml(cfg.Yaml.PathAndFilename)
 	if err != nil {
-		return &Config{}, fmt.Errorf("yaml: %w", err)
+		cfg.YamlConfig = &YamlConfig{} // to avoid nil pointer error without testing all places it is used
+		return cfg, fmt.Errorf("yaml: %w", err)
 	}
 
 	cfg.YamlConfig = yaml
